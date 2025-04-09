b8ff2c470c14c482d5b50edb6581a2f76e64372a
samxssj <samuelbermudez2812@gmail.com>
2025-04-09 16:44:44 +0200

    Configuració per visualitzar el servidor vite desde la xarxa local

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,           // <-- permite acceso desde red local
+    port: 5173,           // <-- el puerto por defecto o el que quieras
+    strictPort: true,     // <-- evita que lo cambie automáticamente
+  }
 })
